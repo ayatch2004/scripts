bddb78eac9e6df4b5f4544de2c775168687a56c9
ayatch2004 <67187048+ayatch2004@users.noreply.github.com>
2025-01-25 00:08:56 +0900

    Create a.js
    first

---
+++ a.js
@@ -0,0 +1,8 @@
+setTimeout(() => {
+    const currencySwitch = document.querySelector('.block-order_position_book > div > div > div > div > div.lw-mb-6.lw-mt-12.lw-w-full > div > div')
+    currencySwitch.click()
+    setTimeout(() => {
+        const USDJPY = document.querySelector('.block-order_position_book > div > div > div > div > div.lw-mb-6.lw-mt-12.lw-w-full > div > ul > li:nth-child(8) > button')
+        USDJPY.click()
+    }, 250);
+}, 250);
