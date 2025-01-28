897474fa50a196575a5c26fba496f861fa83ccb4
ayatch2004 <67187048+ayatch2004@users.noreply.github.com>
2025-01-28 22:47:55 +0900

    fix a suble bug of invisible character

--- a.js
+++ a.js
@@ -1,4 +1,4 @@
-if(⁠window.location.pathname === '/bvi-en/lab-education/tools/order-book-position-book-tool/'){
+if(window.location.pathname === '/bvi-en/lab-education/tools/order-book-position-book-tool/'){
     setTimeout(() => {
         const currencySwitch = document.querySelector('.block-order_position_book > div > div > div > div > div.lw-mb-6.lw-mt-12.lw-w-full > div > div')
         currencySwitch.click()
