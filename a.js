02e3639f7976b8500dfeef1d3ed793c117823d8e
ayatch2004 <67187048+ayatch2004@users.noreply.github.com>
2025-01-28 22:11:07 +0900

    But fix

--- a.js
+++ a.js
@@ -1,6 +1,7 @@
 if(⁠window.location.pathname === '/bvi-en/lab-education/tools/order-book-position-book-tool/'){
     setTimeout(() => {
-        const currencySwitch = document.querySelector('.block-order_position_book > div > div > div > div > div.lw-mb-6.lw-mt-12.lw-w-full > div > div')        currencySwitch.click()
+        const currencySwitch = document.querySelector('.block-order_position_book > div > div > div > div > div.lw-mb-6.lw-mt-12.lw-w-full > div > div')
+        currencySwitch.click()
         setTimeout(() => {
             const USDJPY = document.querySelector('.block-order_position_book > div > div > div > div > div.lw-mb-6.lw-mt-12.lw-w-full > div > ul > li:nth-child(8) > button')
             USDJPY.click()
