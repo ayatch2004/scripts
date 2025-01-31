398060f6d98c95a305b090f5a30511df4e7a13c0
ayatch2004 <67187048+ayatch2004@users.noreply.github.com>
2025-01-31 20:23:25 +0900

    Improvement

--- a.js
+++ a.js
@@ -6,5 +6,5 @@ if(window.location.pathname === '/bvi-en/lab-education/tools/order-book-position
             const USDJPY = document.querySelector('.block-order_position_book > div > div > div > div > div.lw-mb-6.lw-mt-12.lw-w-full > div > ul > li:nth-child(8) > button')
             USDJPY.click()
         }, 250);
-    }, 750);
+    }, 1250);
 }
