907922626e0efb7d3f96d59b35305df557efaa05
ayatch2004 <67187048+ayatch2004@users.noreply.github.com>
2025-02-03 22:20:06 +0900

    little change

--- a.js
+++ a.js
@@ -7,6 +7,6 @@ if(document.readyState === 'complete'){
                 const USDJPY = document.querySelector('.block-order_position_book > div > div > div > div > div:nth-child(2) > div > ul > li:nth-child(8) > button')
                 USDJPY.click()
             }, 250);
-        }, 1250);
+        }, 250);
     }
 }
