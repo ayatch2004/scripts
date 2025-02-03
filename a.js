d2cc2c49e6cc282d742a4d0b196201e53acfa61e
ayatch2004 <67187048+ayatch2004@users.noreply.github.com>
2025-02-03 22:43:32 +0900

    a

--- a.js
+++ a.js
@@ -1,19 +1,19 @@
+function _bodyLoop() {
+    if(document.readyState === 'complete'){   
+        const currencySwitch = document.querySelector('.block-order_position_book > div > div > div > div > div:nth-child(2) > div > div')
+        currencySwitch.click()
+        setTimeout(() => {
+            const USDJPY = document.querySelector('.block-order_position_book > div > div > div > div > div:nth-child(2) > div > ul > li:nth-child(8) > button')
+            USDJPY.click()
+        }, 250);
+
+        return;
+    }
+
+    // loop logic
+    setTimeout(_bodyLoop, 750);
+}
 
 if(window.location.pathname === '/bvi-en/lab-education/tools/order-book-position-book-tool/'){
-    setTimeout(() => {
-        while (true) {
-            if(document.readyState != 'complete')
-                continue;
-
-            const currencySwitch = document.querySelector('.block-order_position_book > div > div > div > div > div:nth-child(2) > div > div')
-            currencySwitch.click()
-
-            setTimeout(() => {
-                const USDJPY = document.querySelector('.block-order_position_book > div > div > div > div > div:nth-child(2) > div > ul > li:nth-child(8) > button')
-                USDJPY.click()
-            }, 250);
-
-            break;
-        }
-    }, 750);
+    setTimeout(_bodyLoop, 750);
 }
