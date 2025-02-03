9e00cf582aee283c5e78720e88a9cd24c1f843ff
ayatch2004 <67187048+ayatch2004@users.noreply.github.com>
2025-02-03 19:19:27 +0900

    Put whole code in Domcontentloaded event

--- a.js
+++ a.js
@@ -1,10 +1,12 @@
-if(window.location.pathname === '/bvi-en/lab-education/tools/order-book-position-book-tool/'){
-    setTimeout(() => {
-        const currencySwitch = document.querySelector('.block-order_position_book > div > div > div > div > div.lw-mb-6.lw-mt-12.lw-w-full > div > div')
-        currencySwitch.click()
+addEventListener("DOMContentLoaded", () => {
+    if(window.location.pathname === '/bvi-en/lab-education/tools/order-book-position-book-tool/'){
         setTimeout(() => {
-            const USDJPY = document.querySelector('.block-order_position_book > div > div > div > div > div.lw-mb-6.lw-mt-12.lw-w-full > div > ul > li:nth-child(8) > button')
-            USDJPY.click()
-        }, 250);
-    }, 1250);
-}
+            const currencySwitch = document.querySelector('.block-order_position_book > div > div > div > div > div.lw-mb-6.lw-mt-12.lw-w-full > div > div')
+            currencySwitch.click()
+            setTimeout(() => {
+                const USDJPY = document.querySelector('.block-order_position_book > div > div > div > div > div.lw-mb-6.lw-mt-12.lw-w-full > div > ul > li:nth-child(8) > button')
+                USDJPY.click()
+            }, 250);
+        }, 1250);
+    }
+});
